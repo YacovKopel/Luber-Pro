b74597ae7ad8afb60cd0c24abdce8d11bf690529
Yacov Kopel <kokokopel94@gmail.com>
2023-06-13 15:52:28 -0700

    configured Google palces autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,58 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
-import React from 'react'
-import tw from 'tailwind-react-native-classnames'
-import NavOptions from '../components/NavOptions'
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { API_KEY } from "@env";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView  style={tw`bg-white h-full`}>
-        <View style={tw `p-5`}>
-            <Image
-            style={{
-                width: 100,
-                height: 100,
-                resizeMode: "contain"
-            }}
-            source={{
-                uri:"https://links.papareact.com/gzs",
-            }}
-                />
-                <NavOptions/>
-                      </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+
+        <GooglePlacesAutocomplete
+          styles={{
+            container: {
+              flex: 3,
+              marginBottom:10
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details=null)=>{
+            
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: API_KEY,
+            language: "en",
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          placeholder="Where From?"
+          debounce={400}
+        />
+    
+
+        <NavOptions />
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
