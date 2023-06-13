130a889f8913de18a71cfec129f82dff62c65fe6
Yacov Kopel <kokokopel94@gmail.com>
2023-06-13 12:50:53 -0700

    added Navoptions with clickable buttons and added tailwindCSS

---
+++ components/NavOptions.js
@@ -0,0 +1,37 @@
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+
+const data=[{
+    id:1,
+    title: "Get A Ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen"
+},
+{   id:2,
+    title: "Order Food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen"
+}]
+
+
+const NavOptions = () => {
+  return (
+    <FlatList
+    data={data}
+    horizontal
+    keyExtractor={(item) => item.id}
+    renderItem={({item})=>
+<TouchableOpacity
+style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+    <View>
+        <Image
+        style={{width:120, height:120, resizeMode: "contain"}}
+        source = {{uri: item.image}}/>
+        <Text style={tw `mt-2 text-lg font-semibold`}>{item.title}</Text>
+</View>
+</TouchableOpacity>}/>
+  )
+}
+
+export default NavOptions
