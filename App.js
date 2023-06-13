af3735f09f3ef354fe787d791a87356ba6513cf2
Yacov Kopel <kokokopel94@gmail.com>
2023-06-13 11:48:22 -0700

    added story.js file

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
 
 
 export default function App() {
   return (
+    <Provider store={}>
     <View style={styles.container}>
       <Text>Welcome to LUber!</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
