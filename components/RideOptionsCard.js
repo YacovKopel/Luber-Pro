08b1bb5632217c54a375a9bd0added0db19fb3da
Yacov Kopel <kokokopel94@gmail.com>
2023-06-19 15:42:06 -0700

    added diffrent ride options images

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,11 +1,63 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
+
+const data= [{
+    id: "lUber-X-123",
+    title:"lUberX",
+    multiplier:1,
+    image: "https://links.papareact.com/3pn",
+},
+{
+    id: "lUber-XL-123",
+    title:"lUberXL",
+    multiplier:1.2,
+    image: "https://links.papareact.com/5w8",
+},
+{
+    id: "lUber-LUX-123",
+    title:"lUber LUX",
+    multiplier:1.75,
+    image: "https://links.papareact.com/7pf",
+},
+]
 
 const RideOptionsCard = () => {
+    const navigation= useNavigation()
   return (
-    <View>
-      <Text>RideOptionsCard</Text>
-    </View>
+    <SafeAreaView style={tw`flex-grow bg-white`}>
+        <View>
+            <TouchableOpacity
+            onPress={ () => navigation.navigate("NavigateCard")}
+            style={tw`absolute top-3 left-5 p-3 z-50 rounded-full`}>
+                <Icon
+                name="chevron-left" type="font-awesome"/>
+            </TouchableOpacity>
+      <Text 
+      style={tw` text-center py-5 text-xl`}
+      >Select a Ride</Text>
+      </View>
+
+      <FlatList
+    data={data}
+    keyExtractor={(item) => item.id}
+    renderItem={({item: {id, title, multiplier,image}, item })=>(
+<TouchableOpacity>
+    <Image
+style={{
+    width: 100,
+    height:100,
+    resizeMode:"contain",
+}}
+source={{uri:image}}
+/>
+</TouchableOpacity>
+)}
+/>
+</SafeAreaView>
   )
 }
 
