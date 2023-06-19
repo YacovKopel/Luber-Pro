ba47c8e18463da1babcf7f5bffbfa68c0accb8c9
Yacov Kopel <kokokopel94@gmail.com>
2023-06-19 16:15:46 -0700

    calling api to get travel time information to be able to access in components

--- components/Map.js
+++ components/Map.js
@@ -2,8 +2,8 @@ import { StyleSheet } from 'react-native'
 import React, { useEffect, useRef } from 'react'
 import MapView, {Marker} from 'react-native-maps'
 import tw from 'tailwind-react-native-classnames'
-import { useSelector } from 'react-redux'
-import { selectDestination, selectOrigin } from '../slices/navSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectDestination, selectOrigin, setTravelTimeInformation } from '../slices/navSlice'
 import MapViewDirections from 'react-native-maps-directions'
 import { API_KEY } from "@env";
 
@@ -12,6 +12,7 @@ const Map = () => {
     const origin= useSelector(selectOrigin)
     const destination =useSelector(selectDestination)
     const mapRef= useRef(null)
+    const dispatch= useDispatch()
 
     useEffect(() => {
         if (!origin || !destination) return;
@@ -21,6 +22,20 @@ const Map = () => {
             edgePadding: {top:50, right:50, bottom:50, left:50},
         })
     }, [origin, destination])
+
+    useEffect(()=>{
+        if (!origin || !destination) return;
+        const getTravelTime= async ()=>{
+fetch(`https:maps.googleapis.com/maps/api/distancematrix/json?
+units=imperial&origin=${origin.description}
+&destination=${destination.description}
+&key=${API_KEY}`)
+.then((res)=> res.json())
+.then((data)=> {dispatch(setTravelTimeInformation(data.rows[0].elements[0]))
+})
+}
+getTravelTime()
+    }), [origin, destination,API_KEY]
 
   return (
     <MapView 
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,81 +1,92 @@
-import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image} from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native'
-import tw from 'tailwind-react-native-classnames'
-import { Icon } from 'react-native-elements'
-import { useNavigation } from '@react-navigation/native'
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image, SafeAreaView
+} from "react-native";
+import React, { useState } from "react";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
-const data= [{
+const data = [
+  {
     id: "LUber-X-123",
-    title:"LUberX",
-    multiplier:1,
+    title: "LUberX",
+    multiplier: 1,
     image: "https://links.papareact.com/3pn",
-},
-{
+  },
+  {
     id: "LUber-XL-123",
-    title:"LUberXL",
-    multiplier:1.2,
+    title: "LUberXL",
+    multiplier: 1.2,
     image: "https://links.papareact.com/5w8",
-},
-{
+  },
+  {
     id: "LUber-LUX-123",
-    title:"LUber LUX",
-    multiplier:1.75,
+    title: "LUber LUX",
+    multiplier: 1.75,
     image: "https://links.papareact.com/7pf",
-},
-]
+  },
+];
 
 const RideOptionsCard = () => {
-    const navigation= useNavigation()
-    const [selected, setSelected]=useState(null)
+  const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
   return (
     <SafeAreaView style={tw`flex-grow bg-white px-3`}>
-        <View>
-            <TouchableOpacity
-            onPress={ () => navigation.navigate("NavigateCard")}
-            style={tw`absolute top-3 left-5 p-3 z-50 rounded-full`}>
-                <Icon
-                name="chevron-left" type="font-awesome"/>
-            </TouchableOpacity>
-      <Text 
-      style={tw` text-center py-5 text-xl`}
-      >Select a Ride</Text>
+      <View>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("NavigateCard")}
+          style={tw`absolute top-3 left-5 p-3 z-50 rounded-full`}
+        >
+          <Icon name="chevron-left" type="font-awesome" />
+        </TouchableOpacity>
+        <Text style={tw` text-center py-5 text-xl`}>Select a Ride</Text>
       </View>
 
       <FlatList
-    data={data}
-    keyExtractor={(item) => item.id}
-    renderItem={({item: {id, title, multiplier,image}, item })=>(
-<TouchableOpacity 
-style={tw`flex-row justify-between items-center px-10 ${id===selected?.id && "bg-gray-200"}`}
-onPress={setSelected(item)}>
-    <Image
-style={{
-    width: 100,
-    height:100,
-    resizeMode:"contain",
-}}
-source={{uri:image}}
-/>
-<View  style={tw`-ml-6`}>
-    <Text  style={tw`text-xl font-semibold`}>{title}</Text>
-    <Text>Travel Time</Text>
-</View>
-<Text style={tw`text-xl`}>$99</Text>
-</TouchableOpacity>
-)}
-/>
-<View>
-    <TouchableOpacity style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
-    disabled={!selected}>
-        <Text
-        style={tw`text-center text-white text-xl`}>Choose {selected?.title}</Text>
-    </TouchableOpacity>
-</View>
-</SafeAreaView>
-  )
-}
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, title, multiplier, image }, item }) => (
+          <TouchableOpacity
+            style={tw`flex-row justify-between items-center px-10 ${
+              id === selected?.id && "bg-gray-200"
+            }`}
+            onPress={setSelected(item)}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel Time</Text>
+            </View>
+            <Text style={tw`text-xl`}>$99</Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View>
+        <TouchableOpacity
+          style={tw`bg-black py-3 m-3 ${!selected && "bg-gray-300"}`}
+          disabled={!selected}
+        >
+          <Text style={tw`text-center text-white text-xl`}>
+            Choose {selected?.title}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default RideOptionsCard
+export default RideOptionsCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
