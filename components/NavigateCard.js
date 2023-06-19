2522bafdf5c1759189bf374445a9e2d4e76db55b
Yacov Kopel <kokokopel94@gmail.com>
2023-06-19 13:11:29 -0700

    using APi to get destination into data layer

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -2,18 +2,41 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { API_KEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from '@react-navigation/native';
 
 const NavigateCard = () => {
+    const dispatch = useDispatch();
+    const navigation =useNavigation()
+
   return (
     <SafeAreaView
     style= {tw `bg-white flex-1`}>
       <Text 
-      style={tw `text-center py-5 text-xl`}>Good Morning Jacob</Text>
+      style={tw `text-center py-5 text-xl`}>Good Morning, Jacob!</Text>
       <View style={tw `border-t border-gray-200 flex-shrink`}>
       <View>
         <GooglePlacesAutocomplete 
         placeholder='Where To?'
         styles={toInputBoxStyles}
+        onPress={(data, details=null) => {
+            dispatch(setDestination({
+                location: details.geometry.location,
+                description: data.description
+            })
+            ) 
+            navigation.navigate("RideOptionsCard")  
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          minLength={2}
+          enablePoweredByContainer={false}
+          query={{
+            key: API_KEY,
+            language: "en",
+          }}
         nearbyPlacesAPI='GooglePlacesSearch'
         debounce={400} />
       </View>
@@ -31,7 +54,7 @@ const toInputBoxStyles = StyleSheet.create({
         flex:0,
     },
     textInput: {
-        backgroundColor:"#00000F",
+        backgroundColor:"lightgray",
         borderRadius:0,
         fontSize:18,
     },
