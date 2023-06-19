56ef26e6f84d25b840980183d8844046fdbc9712
Yacov Kopel <kokokopel94@gmail.com>
2023-06-19 13:38:42 -0700

    map zoom out works

--- components/Map.js
+++ components/Map.js
@@ -1,5 +1,5 @@
 import { StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import MapView, {Marker} from 'react-native-maps'
 import tw from 'tailwind-react-native-classnames'
 import { useSelector } from 'react-redux'
@@ -11,9 +11,20 @@ import { API_KEY } from "@env";
 const Map = () => {
     const origin= useSelector(selectOrigin)
     const destination =useSelector(selectDestination)
+    const mapRef= useRef(null)
+
+    useEffect(() => {
+        if (!origin || !destination) return;
+    
+        mapRef.current.fitToSuppliedMarkers(['origin', 'destination'],
+        {
+            edgePadding: {top:50, right:50, bottom:50, left:50},
+        })
+    }, [origin, destination])
 
   return (
     <MapView 
+    ref={mapRef}
     style={tw`flex-1`}
     mapType='mutedStandard'
     initialRegion={{
@@ -42,6 +53,17 @@ const Map = () => {
         title= "Origin"
         description ={origin.description}
         identifier="origin"
+    />
+)}
+{destination?.location && (
+    <Marker
+        coordinate = {{
+            latitude: destination.location.lat,
+            longitude: destination.location.lng,
+        }}
+        title= "Destination"
+        description ={destination.description}
+        identifier="destination"
     />
 )}
   </MapView>
